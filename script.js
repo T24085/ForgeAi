fa18e0e8c6703943a50293192660d88079f03cde
T24085 <Christoffersent@gmail.com>
2026-02-06 22:52:50 -0600

    Refine cover reveal and layout

--- script.js
+++ script.js
@@ -70,3 +70,92 @@ function typeLineLoop(text, target, speed = 52, pause = 1400) {
 }
 
 typeLineLoop(typedText, typedLine);
+
+const coverReveal = document.getElementById("coverReveal");
+
+if (coverReveal) {
+  const svg = coverReveal.querySelector(".cover-mask-svg");
+  const mask = svg.querySelector("#coverMask");
+  const wrapper = svg.querySelector(".cover-mask-wrapper");
+
+  const pointer = { x: 0, y: 0, smoothX: 0, smoothY: 0, diff: 0, vx: 0, vy: 0 };
+  const particles = [];
+
+  const updateViewBox = () => {
+    const rect = coverReveal.getBoundingClientRect();
+    svg.setAttribute("viewBox", `0 0 ${rect.width} ${rect.height}`);
+    mask.setAttribute("x", "0");
+    mask.setAttribute("y", "0");
+    mask.setAttribute("width", rect.width);
+    mask.setAttribute("height", rect.height);
+  };
+
+  const onPointerMove = (event) => {
+    const rect = coverReveal.getBoundingClientRect();
+    pointer.vx = pointer.x - event.clientX;
+    pointer.vy = pointer.y - event.clientY;
+    pointer.x = event.clientX - rect.left;
+    pointer.y = event.clientY - rect.top;
+  };
+
+  const emitParticle = () => {
+    if (pointer.diff < 0.05) return;
+    const size = Math.min(pointer.diff * 0.45, 70);
+    const particle = new Particle(pointer.smoothX, pointer.smoothY, size);
+    particles.push(particle);
+    wrapper.appendChild(particle.el);
+  };
+
+  class Particle {
+    constructor(x, y, size) {
+      this.x = x;
+      this.y = y;
+      this.size = size;
+      this.life = 1;
+      this.grow = 1;
+      this.el = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+      this.el.setAttribute("cx", this.x);
+      this.el.setAttribute("cy", this.y);
+      this.el.setAttribute("r", this.size);
+      this.el.setAttribute("fill", "white");
+    }
+
+    render(delta) {
+      if (this.life > 0.6) {
+        this.size += this.grow * delta * 0.12;
+      } else {
+        this.size -= delta * 0.18;
+      }
+      this.life -= delta * 0.18;
+      if (this.life <= 0 || this.size <= 0) {
+        this.el.remove();
+        return false;
+      }
+      this.el.setAttribute("r", Math.max(this.size, 0));
+      return true;
+    }
+  }
+
+  const render = () => {
+    pointer.smoothX += (pointer.x - pointer.smoothX) * 0.2;
+    pointer.smoothY += (pointer.y - pointer.smoothY) * 0.2;
+    pointer.diff = Math.hypot(pointer.x - pointer.smoothX, pointer.y - pointer.smoothY);
+
+    emitParticle();
+
+    for (let i = particles.length - 1; i >= 0; i -= 1) {
+      const alive = particles[i].render(1);
+      if (!alive) particles.splice(i, 1);
+    }
+
+    requestAnimationFrame(render);
+  };
+
+  coverReveal.addEventListener("pointermove", onPointerMove);
+  coverReveal.addEventListener("pointerleave", () => {
+    pointer.diff = 0;
+  });
+  window.addEventListener("resize", updateViewBox);
+  updateViewBox();
+  render();
+}
